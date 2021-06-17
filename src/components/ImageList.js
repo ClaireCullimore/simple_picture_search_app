cd58b8f9d780b9c337583b84376298fe81582812
CLaire Cullimore <cascullimore@gmail.com>
2021-06-17 10:56:30 +0100

    add space styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
-import './ImageList.css'
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard'
 
-const ImageList = (props) => {
 
-  const images = props.images.map(({description, id, urls}) => {
-    return <img key={id} src= {urls.regular} alt={description} />;
+const ImageList = props => {
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
 
   });
 
@@ -14,3 +15,17 @@ const ImageList = (props) => {
 };
 
 export default ImageList;
+
+
+// //const ImageList = props => {
+// //   const images = props.images.map(({description, id, urls}) => {
+// //     return <img key={id} alt={description} src={urls.regular} />;
+
+// //   });
+
+// //   return <div className="image-list">{images}</div>;
+
+
+// // };
+
+// // export default ImageList;
